4f8e68f3fe9910bda2fb08aeb2c5ef0dc2ac981f
Patrick Machado <p.machado080493@gmail.com>
2023-05-31 11:10:30 +0400

    landing page totally rock

--- Main.py
+++ Main.py
@@ -41,13 +41,15 @@ img3 = Image.open('Images/yolo.jpg' )
 
 img4 = Image.open('Images/CNN.jpg' )
 
-img1_res = img1.resize((300,300))
+height = 250
 
-img2_res = img2.resize((300,300))
+img1_res = img1.resize((300,height))
 
-img3_res = img3.resize((300,300))
+img2_res = img2.resize((300,height))
 
-img4_res = img4.resize((300,300))
+img3_res = img4.resize((300,height))
+
+img4_res = img3.resize((300,height))
 
 col1,col2, col3, col4 = st.columns(4)
 
@@ -64,10 +66,10 @@ if col2.button("DoctorBot", use_container_width = True):
 
 col3.image(img3_res, use_column_width=True)
 
-if col3.button("Models", use_container_width = True):
-    switch_page(page_name = 'Models')
+if col3.button("Malaria Detection", use_container_width = True):
+    switch_page(page_name = 'Malaria Detection')
 
 col4.image(img4_res, use_column_width=True)
 
-if col4.button("About CNN", use_container_width = True):
-    switch_page(page_name = 'Malaria Project')
+if col4.button("Under the Hood", use_container_width = True):
+    switch_page(page_name = 'Under the Hood')
